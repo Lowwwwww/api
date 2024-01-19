0f7e034ca448bceb6469cac248e9ff690215e121
CLEITON EDUARDO CORREA <cecorrea2007@gmail.com>
2024-01-19 14:48:06 -0300

    Modificação no codigo models/EntradaEstoque.js

--- models/EntradaEstoque.js
+++ models/EntradaEstoque.js
@@ -1,6 +1,6 @@
 class EntradaEstoque {
     constructor(id_entrada, id_produto, quantidade, data_entrada) {
-        this.id_entrada = id_entrada;
+     //   this.id_entrada = id_entrada;
         this.id_produto = id_produto;
         this.quantidade = quantidade;
         this.data_entrada = data_entrada;
